ae6f5c7864e6ab96004e4ce948a7e18375e0b011
Kieran McErlean <kpmcerlean@gmail.com>
2018-02-17 17:46:28 +0000

    Add Echo.java
    Echo's command line arguments

---
+++ src/javapractice/Echo.java
@@ -0,0 +1,15 @@
+package javapractice;
+
+public class Echo {
+	
+	public static void main(String[] args) {
+		
+		
+		for( int num = 0; num<args.length; num++) {
+			System.out.print(args[num] + " ");
+		}
+		System.out.println();
+	}
+}
+			
+		
